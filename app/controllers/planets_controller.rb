7b5b48f04d0f5cce8c959975509dc0da8df156c4
ntuvera <ntuvera@gmail.com>
2014-08-14 23:21:37 -0400

    add create, show, edit actions, needs styling

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -4,22 +4,32 @@ class PlanetsController < ApplicationController
   end
 
   def new
+    @planet = Planet.new
   end
 
   def create
     planet = Planet.new(planet_params)
+    planet.save
+    redirect_to 'planets/#{planet.id}'
   end
 
   def show
+    @planet = Planet.find(params[:id])
   end
 
   def edit
+    @planet = Planet.find(params[:id])
   end
 
   def update
+    planet = Planet.find(params[:id])
+    planet.update(planet_params)
+    redirect_to "/planets/#{planet.id}"
   end
 
   def destroy
+    Planet.delete(params[:id])
+    redirect_to root_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'planets#index'
-  resource :planets
+  resources :planets
 end
